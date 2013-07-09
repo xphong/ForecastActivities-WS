cb142c85c664fd4e3d4f7e031815d0ed721ebab9
xphong <xphong@gmail.com>
2013-07-09 14:03:16 -0400

    Complete

--- ConsumeLocation.php
+++ ConsumeLocation.php
@@ -1,13 +1,56 @@
 <?php
-include('functions.php');
+require('functions.php');
+require('database.php');
+
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Forecast Activities</title>
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA4ommQ8l4W9-aQTNXmgF8G43Y-rvR4t1Y&sensor=true"></script>
+        <script type="text/javascript">
+
+        $(document).ready(function(){
+
+                navigator.geolocation.getCurrentPosition(initialize);
+
+                function initialize(position) {
+
+                    var mapOptions = {
+                        center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                        zoom: 5,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    };
+
+                    var map = new google.maps.Map(document.getElementById("map-canvas"),
+                        mapOptions);
+
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                        map: map,
+                        title:""
+                    });
+                }
+                google.maps.event.addDomListener(window, 'load', initialize);
+            });
+
+    </script>
+    <style>
+    #map-canvas
+    {
+        clear:both;
+        float:left;
+        height:250px;
+        margin-right:15px;
+        padding:25px;
+        width:250px;
+    }
+    </style>
     </head>
     <body>
+        <div id="map-canvas"></div>
         <div>
             <h1>Get Weather Summary From Address</h1>
             <form method="post" action="ConsumeLocation.php">
@@ -18,26 +61,36 @@ include('functions.php');
                 <input type="submit" value="Submit" />
                 <br /><br />
             </form>
-
         </div>
-
 
         <?php
         if ($_POST) {
             $address = $_POST["address"];
-            
-            if ((isset($address) && !empty($address))) {
-                $coordinates = getLatLong($address);
 
+            if ((isset($address) && !empty($address))) {
+                // get coordinates from inputted address
+                $coordinates = getLatLong($address);
                 $lat = $coordinates['lat'];
                 $long = $coordinates['long'];
 
+                // get weather using coordinates
                 $weather_summary = getWeather($lat, $long);
 
+                // output results
                 echo "Weather Summary: $weather_summary<br />";
                 echo "Latitude: $lat<br />";
                 echo "Longitude: $long<br />";
-            } else {
+
+                // get activities from database
+                $activities = getActivities($weather_summary);
+
+                // output activities
+                echo "Activities: ";
+                foreach ($activities as $activity) {
+                    echo "<br />-$activity";
+                }
+            }
+            else {
                 echo "Please enter an address.";
             }
         }
--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// get latitude, longitude from address
 function getLatLong($address) {
     if (!is_string($address))
         die("All Addresses must be passed as a string");
@@ -15,13 +16,45 @@ function getLatLong($address) {
     return $_coords;
 }
 
+// get weather summary from latitude, longitude
 function getWeather($lat, $long) {
+    // retrieve weather from API
     $json = file_get_contents("https://api.forecast.io/forecast/5fbb07cf1d7055ece87605c3f811b50b/$lat,$long");
+    // convert json return
     $data = json_decode($json, TRUE);
 
     $current = $data['currently'];
-
+    
+    // return weather summary
     return $current['summary'];
+}
+
+// get activities from database
+function getActivities($weather) {
+    $db = Database::getDB();
+    // if weather is rainy
+    if ($weather == "Drizzle" || $weather == "Rain" || $weather == "Light Rain") {
+        $query = "SELECT * FROM activities WHERE weather = 'Rain'";
+    }
+    // if weather is cloudy
+    else if ($weather == "Partly Cloudy" || $weather == "Mostly Cloudy" || $weather == "Overcast"){
+        $query = "SELECT * FROM activities WHERE weather = 'Cloudy'";
+        
+    }
+    // if weather is sunny
+    else if ($weather == "Clear" || $weather == "Breezy") {
+         $query = "SELECT * FROM activities WHERE weather = 'Sunny'";       
+    }
+    else {
+        $query = "SELECT * FROM activities";
+    }
+    // database query
+    $result = $db->query($query);
+    $activities = array();
+    foreach ($result as $row) {
+        $activities[] = $row['activity'];
+    }
+    return $activities;
 }
 
 ?>
